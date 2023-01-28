24261e8b846df6fd8eb61522d3a902342099f2b8
Franek Antoniak <franekant123@gmail.com>
2023-01-28 20:42:16 +0100

    Initial commit

---
+++ src/main/java/org/recru/currenda/court/RecruCourtApplication.java
@@ -0,0 +1,13 @@
+package org.recru.currenda.court;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruCourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruCourtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/recru/currenda/court/RecruCourtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.recru.currenda.court;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruCourtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
