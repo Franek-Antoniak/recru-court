c2667ad1286a234e244ab10dc1ae19c71da200ca
Franek Antoniak <franekant123@gmail.com>
2023-01-30 06:01:29 +0100

    fix: repository method fix in jpa case repository

--- src/main/java/org/recru/currenda/court/repository/casee/JpaCaseEntityRepository.java
+++ src/main/java/org/recru/currenda/court/repository/casee/JpaCaseEntityRepository.java
@@ -4,7 +4,6 @@ import org.recru.currenda.court.entity.casee.CaseEntity;
 import org.recru.currenda.court.entity.casee.CaseType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
@@ -16,14 +15,10 @@ import java.util.stream.Collectors;
 @Profile("jpa")
 interface JpaCaseEntityRepository extends CaseEntityRepository, JpaRepository<CaseEntity, Long> {
 	default Map<CaseType, CaseEntity> findByDateOfEntryBetweenGroupByCaseType(LocalDate from, LocalDate to) {
-		List<CaseEntity> caseResponses = findByDateOfEntryBetween(from, to);
+		List<CaseEntity> caseResponses = findAllByDateOfEntryBetween(from, to);
 		return caseResponses.stream()
 				.collect(Collectors.toMap(CaseEntity::getCaseType, Function.identity()));
 	}
 
-	@Query(
-			"SELECT c.caseType as caseType " + "FROM CaseEntity c " + "WHERE c.dateOfEntry >= :from AND c.dateOfEntry "
-					+ "<= :to " + "GROUP BY c.caseType"
-	)
-	List<CaseEntity> findByDateOfEntryBetween(@Param("from") LocalDate from, @Param("to") LocalDate to);
+	List<CaseEntity> findAllByDateOfEntryBetween(@Param("from") LocalDate from, @Param("to") LocalDate to);
 }
